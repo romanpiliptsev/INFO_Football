a915953c0bbcf7ece7ded44ea09ee18d49490a6d
Roman <romazzz777@gmail.com>
2022-10-14 23:30:11 +0300

    Update MainActivity

--- app/src/main/java/com/example/infofootball/MainActivity.kt
+++ app/src/main/java/com/example/infofootball/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.infofootball
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -29,24 +30,66 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        llLogo?.scaleX = 1F; llLogo?.scaleY = 1F
+        blLogo?.scaleX = 1.2F; blLogo?.scaleY = 1.2F
+        saLogo?.scaleX = 1F; saLogo?.scaleY = 1F
+        plLogo?.scaleX = 0.8F; plLogo?.scaleY = 0.8F
+        l1Logo?.scaleX = 0.92F; l1Logo?.scaleY = 0.92F
+        nlLogo?.scaleX = 0.93F; nlLogo?.scaleY = 0.93F
     }
 
     fun onClickLl(view: View) {
+        view.scaleX *= 1.1F
+        view.scaleY *= 1.1F
+
+        val intent = Intent(this, LeagueActivity::class.java)
+        intent.putExtra("leagueId", 302)
+        startActivity(intent)
     }
 
     fun onClickPl(view: View) {
+        view.scaleX *= 1.1F
+        view.scaleY *= 1.1F
+
+        val intent = Intent(this, LeagueActivity::class.java)
+        intent.putExtra("leagueId", 152)
+        startActivity(intent)
     }
 
     fun onClickBl(view: View) {
+        view.scaleX *= 1.1F
+        view.scaleY *= 1.1F
+
+        val intent = Intent(this, LeagueActivity::class.java)
+        intent.putExtra("leagueId", 175)
+        startActivity(intent)
     }
 
     fun onClickL1(view: View) {
+        view.scaleX *= 1.1F
+        view.scaleY *= 1.1F
+
+        val intent = Intent(this, LeagueActivity::class.java)
+        intent.putExtra("leagueId", 168)
+        startActivity(intent)
     }
 
     fun onClickNl(view: View) {
+        view.scaleX *= 1.05F
+        view.scaleY *= 1.05F
+
+        val intent = Intent(this, LeagueActivity::class.java)
+        intent.putExtra("leagueId", 244)
+        startActivity(intent)
     }
 
     fun onClickSa(view: View) {
+        view.scaleX *= 1.1F
+        view.scaleY *= 1.1F
+
+        val intent = Intent(this, LeagueActivity::class.java)
+        intent.putExtra("leagueId", 207)
+        startActivity(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
