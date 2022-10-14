c9df3ef5468e23b393b1771bfa9c38c938951e74
Roman <romazzz777@gmail.com>
2022-10-15 00:12:33 +0300

    Update Table&TopScorersAdapter

--- app/src/main/java/com/example/infofootball/adapters/TableAdapter.kt
+++ app/src/main/java/com/example/infofootball/adapters/TableAdapter.kt
@@ -16,17 +16,52 @@ import com.squareup.picasso.Picasso
 class TableAdapter(private val tableItems: List<TableItem>) : RecyclerView.Adapter<TableAdapter.TableViewHolder>() {
 
     override fun getItemCount(): Int {
-        return 0
+        return tableItems.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TableViewHolder {
-        return TableViewHolder(parent)
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.table_item, parent, false)
+        return TableViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TableViewHolder, position: Int) {
+        val tableItem = tableItems[position]
 
+        val logo = tableItem.logo
+        if (logo.endsWith("svg")) {
+            GlideToVectorYou
+                .init()
+                .with(holder.getContext())
+                .load(Uri.parse(logo), holder.logo)
+        } else {
+            Picasso.get().load(logo).into(holder.logo)
+        }
+
+        holder.position.text = tableItem.position.toString()
+        holder.name.text = tableItem.name
+        holder.games.text = tableItem.playedGames.toString()
+        holder.wins.text = tableItem.won.toString()
+        holder.draws.text = tableItem.draw.toString()
+        holder.defeat.text = tableItem.lost.toString()
+        val diff = (tableItem.goalsFor - tableItem.goalsAgainst).toString()
+        holder.difference.text = if (diff.startsWith("-") || diff == "0") diff else "+$diff"
+        holder.points.text = tableItem.points.toString()
     }
 
     class TableViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val position: TextView = itemView.findViewById(R.id.position)
+        val logo: ImageView = itemView.findViewById(R.id.logo)
+        val name: TextView = itemView.findViewById(R.id.name)
+        val games: TextView = itemView.findViewById(R.id.games)
+        val wins: TextView = itemView.findViewById(R.id.wins)
+        val draws: TextView = itemView.findViewById(R.id.draws)
+        val defeat: TextView = itemView.findViewById(R.id.defeat)
+        val difference: TextView = itemView.findViewById(R.id.difference)
+        val points: TextView = itemView.findViewById(R.id.points)
+
+        fun getContext(): Context {
+            return itemView.context
+        }
     }
 }
--- app/src/main/java/com/example/infofootball/adapters/TopScorersAdapter.kt
+++ app/src/main/java/com/example/infofootball/adapters/TopScorersAdapter.kt
@@ -13,16 +13,40 @@ class TopScorersAdapter(private val topScorers: List<TopScorer>)
     : RecyclerView.Adapter<TopScorersAdapter.TopScorerViewHolder>() {
 
     override fun getItemCount(): Int {
-        return 0
+        return topScorers.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopScorerViewHolder {
-        return TopScorerViewHolder(parent)
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.topscorer_item, parent, false)
+        return TopScorerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TopScorerViewHolder, position: Int) {
+        val topScorerItem = topScorers[position]
+
+        holder.place.text = "${topScorerItem.place}"
+        holder.name.text = topScorerItem.name
+        holder.team.text = topScorerItem.teamName
+        holder.goals.text = "GOALS: ${topScorerItem.goals}"
+        holder.assists.text = "A: ${topScorerItem.assists}"
+        holder.penGoals.text = "PEN: ${topScorerItem.penGoals}"
+
+//        Picasso.get().load(JSONUtils.getPlayerPhotoFromJSON(NetworkUtils.getJSONForPlayer(topScorerItem.playerKey ?: 0))).placeholder(R.drawable.placeholder).into(holder.photo)
+//        Picasso.get().load("asdasdasf.com/hahahahlol").placeholder(R.drawable.placeholder).into(holder.photo)
     }
 
     class TopScorerViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.topscorer_name)
+        val team: TextView = itemView.findViewById(R.id.topscorer_team)
+        val goals: TextView = itemView.findViewById(R.id.top_scorer_goals)
+        val assists: TextView = itemView.findViewById(R.id.top_scorer_assists)
+        val penGoals: TextView = itemView.findViewById(R.id.top_scorer_pen_goals)
+        val place: TextView = itemView.findViewById(R.id.top_scorer_position)
+//        val photo: ImageView = itemView.findViewById(R.id.top_scorer_player_photo)
+
+        fun getContext(): Context {
+            return itemView.context
+        }
     }
 }
