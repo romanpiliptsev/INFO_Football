868e2cace3586d7769c92244df30b32617d06ea9
Roman <romazzz777@gmail.com>
2022-10-15 00:38:18 +0300

    Update VideosAdapter

--- app/src/main/java/com/example/infofootball/adapters/VideosAdapter.kt
+++ app/src/main/java/com/example/infofootball/adapters/VideosAdapter.kt
@@ -12,17 +12,22 @@ class VideosAdapter(private val videos: List<Video>)
     : RecyclerView.Adapter<VideosAdapter.VideoViewHolder>() {
 
     override fun getItemCount(): Int {
-        return 0
+        return videos.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
-        return VideoViewHolder(parent)
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.video_item, parent, false)
+        return VideoViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+        val videoItem = videos[position]
 
+        holder.title.text = videoItem.title
     }
 
     class VideoViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.title)
     }
 }
