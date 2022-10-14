31fb246099ba06bd87e3941c9600e880a95867aa
Roman <romazzz777@gmail.com>
2022-10-15 01:01:27 +0300

    Update FavoritesAdapter

--- app/src/main/java/com/example/infofootball/adapters/FavoritesAdapter.kt
+++ app/src/main/java/com/example/infofootball/adapters/FavoritesAdapter.kt
@@ -17,19 +17,38 @@ class FavoritesAdapter(private val teams: List<DBTeam>)
     : RecyclerView.Adapter<FavoritesAdapter.FavoritesViewHolder>() {
 
     override fun getItemCount(): Int {
-        return 0
+        return teams.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoritesViewHolder {
-
-        return FavoritesViewHolder(parent)
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.favorite_item, parent, false)
+        return FavoritesViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: FavoritesViewHolder, position: Int) {
+        val favoriteItem = teams[position]
 
+        val logo = favoriteItem.getTeamLogo()
+
+        if (logo.endsWith("svg")) {
+            GlideToVectorYou
+                .init()
+                .with(holder.getContext())
+                .load(Uri.parse(logo), holder.favoriteLogo)
+        } else {
+            Picasso.get().load(logo).into(holder.favoriteLogo)
+        }
+
+        holder.favoriteName.text = favoriteItem.getTeamName()
     }
 
     class FavoritesViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val favoriteLogo: ImageView = itemView.findViewById(R.id.favorite_logo)
+        val favoriteName: TextView = itemView.findViewById(R.id.favorite_name)
 
+        fun getContext(): Context {
+            return itemView.context
+        }
     }
 }
