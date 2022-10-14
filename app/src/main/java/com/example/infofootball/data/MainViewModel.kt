8fd8c7a19f3d948bd4f0bf761da2f31c32c95a92
Roman <romazzz777@gmail.com>
2022-10-15 00:50:36 +0300

    Update database: add necessary methods

--- app/src/main/java/com/example/infofootball/data/MainViewModel.kt
+++ app/src/main/java/com/example/infofootball/data/MainViewModel.kt
@@ -16,22 +16,62 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun getFavorites(): List<DBTeam>? {
-        return null
+        return GetAllFavoritesTask().execute().get()
+    }
+
+    private class GetAllFavoritesTask : AsyncTask<Void, Void, List<DBTeam>?>() {
+        override fun doInBackground(vararg p0: Void): List<DBTeam>? {
+            return database?.favoritesDao()?.getAllFavorites()
+        }
     }
 
     fun deleteAllFavorites() {
+        DeleteAllFavoritesTask().execute()
+    }
 
+    private class DeleteAllFavoritesTask : AsyncTask<Void, Void, Void?>() {
+        override fun doInBackground(vararg p0: Void): Void? {
+            database?.favoritesDao()?.deleteAllFavorites()
+            return null
+        }
     }
 
     fun insertFavorite(team: DBTeam) {
+        InsertFavoriteTask().execute(team)
+    }
 
+    private class InsertFavoriteTask : AsyncTask<DBTeam, Void, Void?>() {
+        override fun doInBackground(vararg p0: DBTeam): Void? {
+            database?.favoritesDao()?.insertFavorite(p0[0])
+            return null
+        }
     }
 
     fun deleteFavorite(team: DBTeam) {
+        DeleteFavoriteTask().execute(team)
+    }
 
+    private class DeleteFavoriteTask : AsyncTask<DBTeam, Void, Void?>() {
+        override fun doInBackground(vararg p0: DBTeam): Void? {
+            database?.favoritesDao()?.deleteFavorite(p0[0])
+            return null
+        }
     }
 
     fun getFavoriteById(id: Int): DBTeam? {
+        try {
+            return GetFavoriteByIdTask().execute(id).get()
+        } catch (e: ExecutionException) {
+            e.printStackTrace()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
         return null
+    }
+
+    private class GetFavoriteByIdTask : AsyncTask<Int, Void, DBTeam?>() {
+        override fun doInBackground(vararg p0: Int?): DBTeam? {
+            return database?.favoritesDao()?.getFavoriteById(p0[0] ?: 0)
+        }
     }
 }
