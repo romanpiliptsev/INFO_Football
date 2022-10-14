cf69af8c48552d721c565e3171f9b20e2c133e48
Roman <romazzz777@gmail.com>
2022-10-15 00:21:16 +0300

    Update Matches&PlayersAdapter

--- app/src/main/java/com/example/infofootball/adapters/MatchesAdapter.kt
+++ app/src/main/java/com/example/infofootball/adapters/MatchesAdapter.kt
@@ -20,18 +20,82 @@ class MatchesAdapter(private val matches: List<Match>, private val team: Team?, 
     : RecyclerView.Adapter<MatchesAdapter.MatchViewHolder>() {
 
     override fun getItemCount(): Int {
-        return 0
+        return matches.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchViewHolder {
-        return MatchViewHolder(parent)
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.match_item, parent, false)
+        return MatchViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MatchViewHolder, position: Int) {
+        val matchItem = matches[position]
 
+        if(matchItem.homeTeamKey == team?.team_key) {
+            holder.firstTeamName.text = name
+            holder.secondTeamName.text = matchItem.awayTeam
+            var logo: String? = team.team_logo
+
+            if (logo?.endsWith("svg") == true) {
+                GlideToVectorYou
+                    .init()
+                    .with(holder.getContext())
+                    .load(Uri.parse(logo), holder.firstTeamLogo)
+            } else {
+                Picasso.get().load(logo).into(holder.firstTeamLogo)
+            }
+
+            logo = JSONUtils.getTeamFromJSON(NetworkUtils.getJSONForTeam(matchItem.awayTeamKey))
+                ?.team_logo
+
+            if (logo?.endsWith("svg") == true) {
+                GlideToVectorYou
+                    .init()
+                    .with(holder.getContext())
+                    .load(Uri.parse(logo), holder.secondTeamLogo)
+            } else {
+                Picasso.get().load(logo).into(holder.secondTeamLogo)
+            }
+        } else {
+            holder.firstTeamName.text = matchItem.homeTeam
+            holder.secondTeamName.text = name
+            var logo: String? = team?.team_logo
+
+            if (logo?.endsWith("svg") == true) {
+                GlideToVectorYou
+                    .init()
+                    .with(holder.getContext())
+                    .load(Uri.parse(logo), holder.secondTeamLogo)
+            } else {
+                Picasso.get().load(logo).into(holder.secondTeamLogo)
+            }
+
+            logo = JSONUtils.getTeamFromJSON(NetworkUtils.getJSONForTeam(matchItem.homeTeamKey))
+                ?.team_logo
+
+            if (logo?.endsWith("svg") == true) {
+                GlideToVectorYou
+                    .init()
+                    .with(holder.getContext())
+                    .load(Uri.parse(logo), holder.firstTeamLogo)
+            } else {
+                Picasso.get().load(logo).into(holder.firstTeamLogo)
+            }
+        }
+
+        holder.result.text = matchItem.finalResult
     }
 
     class MatchViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val firstTeamLogo: ImageView = itemView.findViewById(R.id.first_team_logo)
+        val firstTeamName: TextView = itemView.findViewById(R.id.first_team_name)
+        val secondTeamLogo: ImageView = itemView.findViewById(R.id.second_team_logo)
+        val secondTeamName: TextView = itemView.findViewById(R.id.second_team_name)
+        val result: TextView = itemView.findViewById(R.id.result)
 
+        fun getContext(): Context {
+            return itemView.context
+        }
     }
 }
--- app/src/main/java/com/example/infofootball/adapters/PlayersAdapter.kt
+++ app/src/main/java/com/example/infofootball/adapters/PlayersAdapter.kt
@@ -15,18 +15,39 @@ class PlayersAdapter(private val players: List<Player>)
     : RecyclerView.Adapter<PlayersAdapter.PlayerViewHolder>() {
 
     override fun getItemCount(): Int {
-        return 0
+        return players.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerViewHolder {
-        return PlayerViewHolder(parent)
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.player_item, parent, false)
+        return PlayerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: PlayerViewHolder, position: Int) {
+        val playerItem = players[position]
 
+        holder.nameAndNumber.text = "${playerItem.name} (${playerItem.number})"
+        holder.positionAndAge.text = "${playerItem.position}, ${playerItem.age} y.o."
+        holder.matches.text = "M: ${playerItem.match_played}"
+        holder.goals.text = "G: ${playerItem.goals}"
+        holder.yc.text = "YC: ${playerItem.yellow_cards}"
+        holder.rc.text = "RC: ${playerItem.red_cards}"
+
+        Picasso.get().load(playerItem.image).placeholder(R.drawable.placeholder).into(holder.photo)
     }
 
     class PlayerViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameAndNumber: TextView = itemView.findViewById(R.id.player_name_and_number)
+        val positionAndAge: TextView = itemView.findViewById(R.id.player_position_and_age)
+        val matches: TextView = itemView.findViewById(R.id.matches)
+        val goals: TextView = itemView.findViewById(R.id.goals)
+        val yc: TextView = itemView.findViewById(R.id.yc)
+        val rc: TextView = itemView.findViewById(R.id.rc)
+        val photo: ImageView = itemView.findViewById(R.id.player_photo)
 
+        fun getContext(): Context {
+            return itemView.context
+        }
     }
 }
